f2e7ac5c680326d707c1c97b9adb97d248a78ac8
Michelle Cinello <m.cinello@gmail.com>
2017-10-27 14:41:19 -0400

    tested lift_off method

--- rocket_test.rb
+++ rocket_test.rb
@@ -8,11 +8,41 @@ class RocketTest < Minitest::Test
     @rocket = Rocket.new
   end
 
-  def test_initialize_random_name
+  def test_change_name
     result = @rocket.name=("Michelle")
 
     expected = "Michelle"
     assert_equal expected, result
   end
+
+  def test_change_colour
+    result = @rocket.colour=("Blue")
+    expected = "Blue"
+    assert_equal expected, result
+  end
+
+  def test_lift_off_if_rocket_not_flying
+    @rocket = Rocket.new(flying: false)
+    result = @rocket.lift_off
+    expected = true
+    assert_equal(@rocket.flying?, true)
+    assert_equal expected, result
+  end
+
+  def test_lift_off_if_rocket_is_already_flying
+    @rocket = Rocket.new(flying: true)
+    result = @rocket.lift_off
+
+    expected = false
+    assert_equal expected, result
+  end
+
+
+  #
+  # def test_land_true
+  #   result = @rocket.land
+  #   expected = true
+  #   assert_equal expected, result
+  # end
 
 end
