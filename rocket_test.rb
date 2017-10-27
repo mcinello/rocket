243187df24878fcbe19b7585107b3af74fb82c4e
Michelle Cinello <m.cinello@gmail.com>
2017-10-27 12:57:28 -0400

    tested if name method works

--- rocket_test.rb
+++ rocket_test.rb
@@ -3,4 +3,16 @@ require_relative "rocket"
 
 class RocketTest < Minitest::Test
   # Write your tests here!
+
+  def setup
+    @rocket = Rocket.new
+  end
+
+  def test_initialize_random_name
+    result = @rocket.name=("Michelle")
+
+    expected = "Michelle"
+    assert_equal expected, result
+  end
+
 end
